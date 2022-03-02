202743664cc4bf003bc57cfad7a3ddb1832c7f60
hrishikesh <hrishikesh.muley@gmail.com>
2022-03-02 18:57:52 +0530

    first time project share initial commit KYC

---
+++ amedeakyc/src/main/java/com/amedea/kyc/AmedeakycApplication.java
@@ -0,0 +1,13 @@
+package com.amedea.kyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmedeakycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmedeakycApplication.class, args);
+	}
+
+}
