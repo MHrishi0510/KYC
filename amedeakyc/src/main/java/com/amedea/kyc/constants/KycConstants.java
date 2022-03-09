aa2ebeabfcd17319584688443fdaf63d60f63579
Alienware <Alienware@Swap>
2022-03-09 18:46:58 +0530

    Msg Added

--- amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
+++ amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
@@ -5,5 +5,7 @@ public class KycConstants {
 	public static final String WELCOME_URL = "/";
 	public static final String WELCOME_MESSAGE = "Welcome to the KYC API";
 	public static final String GET_CLIENT = "/getAllClient";
+	
+	public static String MESSAGE="Hello"
 
 }
