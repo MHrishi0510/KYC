37d5f0add62dd8a54c6276db428221911ba03b00
Amit1 <Amit1@Amit>
2022-03-09 10:42:35 -0800

    #Updated and Commit

--- amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
+++ amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
@@ -5,11 +5,10 @@ public class KycConstants {
 	public static final String WELCOME_URL = "/";
 	public static final String WELCOME_MESSAGE = "Welcome to the KYC API";
 	public static final String GET_CLIENT = "/getAllClient";
-<<<<<<< HEAD
 	
-	public static String MESSAGE="Hello"
+	public static String MESSAGE="Hello";
 
-=======
+
 	public static final String ERROR ="error";
 	public static final String DATA_NOT_FOUND = "data not found !";
 	public static final String HOME_URI="/home";
