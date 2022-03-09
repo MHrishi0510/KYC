c6eb998babfd017c8bcbd8f8309d527a94057537
Alienware <Alienware@Swap>
2022-03-09 19:17:17 +0530

    Msg updated

--- amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
+++ amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
@@ -6,5 +6,7 @@ public class KycConstants {
 	public static final String WELCOME_MESSAGE = "Welcome to the KYC API";
 	public static final String GET_CLIENT = "/getAllClient";
 	public static final String DATA_NOT_FOUND = "data not found !";
+	
+	public static final String MESSAGE = "Hello";
 
 }
