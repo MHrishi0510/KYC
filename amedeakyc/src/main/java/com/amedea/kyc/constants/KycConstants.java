e249e1a223215b1333bb8356490cfd594d96f1e5
darsh <darsh@DESKTOP-DL374AP.bbrouter>
2022-03-09 18:29:24 +0530

    Error Added in constants

--- amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
+++ amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
@@ -5,5 +5,6 @@ public class KycConstants {
 	public static final String WELCOME_URL = "/";
 	public static final String WELCOME_MESSAGE = "Welcome to the KYC API";
 	public static final String GET_CLIENT = "/getAllClient";
-
+	public static final String ERROR ="error";
+	
 }
