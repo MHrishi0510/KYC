f44c92c7c128dbe8a5dd68a3e19d73c62c7434fb
Charu <Charu@Lenovo-PC>
2022-03-09 18:46:25 +0530

    commit 1

--- amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
+++ amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
@@ -9,4 +9,7 @@ public class KycConstants {
 	public static final String DATA_NOT_FOUND = "data not found !";
 	
 	public static final String HOME_URI="/home";
+
+	public static final String USER_INFO="/info";
+	
 }
