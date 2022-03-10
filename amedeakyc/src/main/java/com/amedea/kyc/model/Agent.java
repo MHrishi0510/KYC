3ac171f89a7481872092dd877d0da05979835a04
hp <hp@LAPTOP-GRSSI9HT>
2022-03-10 17:08:18 +0530

    changes in Agent.java

--- amedeakyc/src/main/java/com/amedea/kyc/model/Agent.java
+++ amedeakyc/src/main/java/com/amedea/kyc/model/Agent.java
@@ -5,52 +5,67 @@ public class Agent {
 	private Integer AgentId;
 	private String AgentName;
 	private String AgentCity;
-	
+	private String AgentPhoneNo;
 	public Agent() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	public Agent(Integer agentId, String agentName, String agentCity) {
+	
+	
+	public Agent(Integer agentId, String agentName, String agentCity, String agentPhoneNo) {
 		super();
 		AgentId = agentId;
 		AgentName = agentName;
 		AgentCity = agentCity;
+		AgentPhoneNo = agentPhoneNo;
 	}
+
 
 	public Integer getAgentId() {
 		return AgentId;
 	}
 
+
 	public void setAgentId(Integer agentId) {
 		AgentId = agentId;
 	}
+
 
 	public String getAgentName() {
 		return AgentName;
 	}
 
+
 	public void setAgentName(String agentName) {
 		AgentName = agentName;
 	}
+
 
 	public String getAgentCity() {
 		return AgentCity;
 	}
 
+
 	public void setAgentCity(String agentCity) {
 		AgentCity = agentCity;
 	}
 
+
+	public String getAgentPhoneNo() {
+		return AgentPhoneNo;
+	}
+
+
+	public void setAgentPhoneNo(String agentPhoneNo) {
+		AgentPhoneNo = agentPhoneNo;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Agent [AgentId=" + AgentId + ", AgentName=" + AgentName + ", AgentCity=" + AgentCity + "]";
+		return "Agent [AgentId=" + AgentId + ", AgentName=" + AgentName + ", AgentCity=" + AgentCity + ", AgentPhoneNo="
+				+ AgentPhoneNo + "]";
 	}
 	
 	
-	
-	
-	
-	
-
 }
