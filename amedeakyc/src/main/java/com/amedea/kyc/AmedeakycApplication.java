e637510b30f011443ff4feaa327d73941f6387c7
BhavnaDevgude <bhavna.devgude44@gmail.com>
2022-03-11 16:51:51 +0530

    Initial commit

---
+++ amedeakyc/src/main/java/com/amedea/kyc/AmedeakycApplication.java
@@ -0,0 +1,14 @@
+package com.amedea.kyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AmedeakycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmedeakycApplication.class, args);
+	}
+
+}
---
+++ amedeakyc/src/main/java/com/amedea/kyc/constants/KycConstants.java
@@ -0,0 +1,19 @@
+package com.amedea.kyc.constants;
+
+public class KycConstants {
+
+	public static final String WELCOME_URL = "/";
+	public static final String WELCOME_MESSAGE = "Welcome to the KYC API";
+	public static final String GET_CLIENT = "/getAllClient";
+	public static final String ERROR = "error";
+	public static final String DATA_NOT_FOUND = "data not found !";
+	public static final String MESSAGE = "Hello";
+	public static final String HOME_URI = "/home";
+	public static final String USER_INFO = "/info";
+	public static final String ERROR_CODE = "error code";
+	public static final String ERROR_MESSAGE = "error message";
+	public static final String STATUS_CODE = "status code";
+	public static final String NOT_APPLICABLE = "NA";
+	public static final String WELCOME_USER = "NA";
+
+}
